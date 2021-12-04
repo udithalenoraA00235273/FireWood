03806e26d00e172f5133b320cf7af2a04c2ff670
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-12-03 19:33:14 -0500

    Add project files.

---
+++ FireWood/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FireWood.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
