885fbcc90010515e068a9457098330ba9dd76dac
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-12-03 19:36:55 -0500

    Setting up Models

---
+++ FireWood/Models/Services.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireWood.Models
+{
+    public class Services
+    {
+        [Key]
+        public string ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceDesc { get; set; }
+
+    }
+}
