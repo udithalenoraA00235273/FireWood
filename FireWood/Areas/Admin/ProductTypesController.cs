29aa26ce5950685b45b7b5c30ea912d8c06e3f59
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-12-11 10:16:01 -0500

    Setting up the Admin screen for product types

---
+++ FireWood/Areas/Admin/ProductTypesController.cs
@@ -0,0 +1,24 @@
+﻿using FireWood.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireWood.Areas.Admin
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db;
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            // var data = _db.ProductTypes.ToList();
+            return View(_db.ProductTypes.ToList());
+        }
+    }
+}
