043c7150e0780c192c50447c802a4c2ff6247efc
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-12-17 01:03:00 -0500

    Setting up scaffolding for user logins

--- FireWood/Areas/Customer/Controllers/HomeController.cs
+++ FireWood/Areas/Customer/Controllers/HomeController.cs
@@ -35,5 +35,19 @@ namespace FireWood.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult Detail(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(a => a.ProductTypes).FirstOrDefault(a => a.Id == id);
+            if(product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
     }
 }
