1f6d9f4c8928f73892eede200a7622db0c4f8fe0
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-12-21 09:14:02 -0500

    Setting up integration tests for User login and registration

---
+++ FireWoodIntegrationTest/UserLoginTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace FireWoodIntegrationTest
+{
+    [TestClass]
+    public class UserLoginTest
+    {
+
+        public IWebDriver _webDriver;
+        [TestInitialize]
+        public void StartUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _webDriver = new ChromeDriver();
+        }
+        [TestMethod]
+        public void ToTestRegisterUser()
+        {
+            _webDriver.Navigate().GoToUrl("https://localhost:44330/Identity/Account/Login");
+
+        }
+
+
+        [TestCleanup]
+        public void ShutDown()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
