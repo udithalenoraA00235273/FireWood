f09fdd43203d314d20966250d61be7c4bff3bc28
udithalenoraA00235273 <A00235273@mycambrian.ca>
2021-12-21 21:14:02 -0500

    Completed integration tests for products controller

--- FireWoodIntegrationTest/ProductTypesTest.cs
+++ FireWoodIntegrationTest/ProductTypesTest.cs
@@ -26,18 +26,22 @@ namespace FireWoodIntegrationTest
             _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/ProductTypes/Create");
             _webDriver.FindElement(By.Id("ProductType")).SendKeys("Small Products");
             _webDriver.FindElement(By.CssSelector(".btn-primary")).Click();
+            var URL = _webDriver.Url;
 
-            var output = _webDriver.FindElement(By.XPath("//span[@class='text-danger field-validation-error']"));
-
-            Assert.AreEqual("The Product Type field is required.", output.);
+            Assert.AreEqual(URL, "https://localhost:44330/Admin/ProductTypes");
         }
 
 
         [TestMethod]
-        public void ToTestEditProductType()
+        public void ToTestWeatherTheProductItemGotEdited()
         {
             _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/ProductTypes/Edit");
-           
+            _webDriver.FindElement(By.Id("ProductType")).SendKeys("Medium Test");
+            _webDriver.FindElement(By.CssSelector(".btn-primary")).Click();
+            var URL = _webDriver.Url;
+
+            Assert.AreEqual(URL, "https://localhost:44330/Admin/ProductTypes");
+
 
         }
         [TestMethod]
--- FireWoodIntegrationTest/ProductsTest.cs
+++ FireWoodIntegrationTest/ProductsTest.cs
@@ -10,30 +10,40 @@ namespace FireWoodIntegrationTest
     public class ProductsTest : TestBase
     {
         [TestMethod]
-        public void ToTestCreateProducts()
+        public void ToTestWeatherUserGoingToSaveNullProducts()
         {
             _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/Products/Create");
-            
+            _webDriver.FindElement(By.Id("Name")).SendKeys("");
+            _webDriver.FindElement(By.CssSelector(".btn-primary")).Click();
+
+            var output = _webDriver.FindElement(By.XPath("//span[@class='text-danger field-validation-error']"));
+
+            Assert.AreEqual("The Name field is required.", output.Text);
         }
-        [TestMethod]
-        public void ToTestEditProducts()
+
+        [TestMethod]           
+        public void ToTestWeatherTheProductDetailsCanEdit()
         {
-            _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/Products/Edit");
+            _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/Products/Edit/6");
+            _webDriver.FindElement(By.Id("Name")).SendKeys("Update Name");
+            _webDriver.FindElement(By.CssSelector(".btn-primary")).Click();
+            var URL = _webDriver.Url;
+
+            Assert.AreEqual(URL, "https://localhost:44330/Admin/Products");
 
 
         }
-        [TestMethod]
-        public void ToTestDetailsProducts()
-        {
-            _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/Products/Details");
-
-
-        }
-        [TestMethod]
+       
+        // This test isn't working because for some reason my file paths are not configured accordingly for products controller
+        // I will be really greatful if you can assist me with that file path thing.
+       [TestMethod]
         public void ToTestDeleteProducts()
         {
-            _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/Products/Delete");
+            _webDriver.Navigate().GoToUrl("https://localhost:44330/Admin/Products/Delete/10");
+            _webDriver.FindElement(By.CssSelector(".btn-danger")).Click();
+            var URL = _webDriver.Url;
 
+            Assert.AreEqual(URL, "https://localhost:44330/Admin/Products");
 
         }
     }
